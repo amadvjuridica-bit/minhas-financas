b6634532886f50fecf39c4eb75eeb9a09f62ac32
Luigi <amadvjuridica@gmail.com>
2026-01-13 15:45:39 -0400

    fix: force full-screen layout + global css + logo png

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./global.css"; // <-- FORÇA o CSS correto
 import App from "./App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
